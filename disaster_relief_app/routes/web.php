249227caf56a0911a5585c38a7b11786322db415
rokon <asaduzzamanrokon11@gmail.com>
2026-05-24 02:04:43 +0600

    laravel_framework_added(rokon)

---
+++ disaster_relief_app/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('welcome');
+});
+
+Route::any('/signin', function () {
+    return view('signin');
+});
+
+Route::any('/signup', function () {
+    return view('signup');
+});
+
+$pages = [
+    'admin_dashboard', 'affected_dashboard', 'affected_login', 'affected_register',
+    'camp_manager_dashboard', 'campaigns', 'donate', 'donor_dashboard',
+    'guest', 'logout', 'volunteer_dashboard'
+];
+
+foreach ($pages as $page) {
+    Route::any('/' . $page, function () use ($page) {
+        return view($page);
+    });
+}
